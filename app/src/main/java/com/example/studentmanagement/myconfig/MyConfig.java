38bce385dc4671c8249c6037d7e5fa55501cb988
spn2 <spn2@spn2s-MacBook-Pro.local>
2019-08-08 19:23:01 +0600

    Initial commit

---
+++ app/src/main/java/com/example/studentmanagement/myconfig/MyConfig.java
@@ -0,0 +1,8 @@
+package com.example.studentmanagement.myconfig;
+
+public class MyConfig {
+
+    final public static String LOGIN_URL = "http://infixedu.com/api/login?email=spn5@spondonit.com&password=1234";
+    final public static String STUDENT_LIST = "http://infixedu.com/api/student-list";
+
+}
